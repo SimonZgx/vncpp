c9283bc63f0de1aa142200c1eee4614763e1d13d
SimonZgx <simonzgx@gmail.com>
2021-01-16 13:41:23 +0800

    重构开始

--- src/include/Test.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Simon on 2020/1/5.
-//
-
-#include "Test.h"
-#include <stdio.h>
-void Test::SayHello() {
-    fprintf(0, "Hello");
-}
--- test/test_libevent/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "nonblocking.hpp"
-#include "log.hpp"
-
-int main(int c, char **v) {
-    setvbuf(stdout, nullptr, _IONBF, 0);
-    run();
-    return 0;
-}
