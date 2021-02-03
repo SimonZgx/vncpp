291278d357af2f4468f701421fa673d263be8d4c
zhangxu.0718 <zhangxu.0718@bytedance.com>
2021-02-03 23:50:50 +0800

    Optimized the project structure

--- test/lock_free_queue/main.cc
+++ test/lock_free_queue/main.cc
@@ -5,12 +5,44 @@
 #include <boost/lockfree/queue.hpp>
 
 #include <iostream>
+#include <vector>
+#include <thread>
 
 using namespace std;
 
+boost::lockfree::queue<int> queue_(1000);
+
+void write(int i) {
+    for (;;)
+        queue_.push(i);
+}
+
+void read() {
+    for (;;) {
+        int val;
+        queue_.pop(val);
+        std::cout << val << endl;
+    }
+}
 
 int main() {
 
-    boost::lockfree::queue<int> queue_(100);
-    cout << queue_.is_lock_free() << endl;
+    vector<thread> ths;
+    ths.reserve(6);
+    for (int i = 0; i < 3; ++i) {
+        ths.emplace_back(thread([i]() -> void {
+            write(i);
+        }));
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        ths.emplace_back(thread([]() -> void {
+            read();
+        }));
+    }
+
+    for (auto &item : ths) {
+        item.join();
+    }
+
 }
